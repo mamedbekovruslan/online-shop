d76093f75056f0c71ed3d556ab34e03fa1008b86
mamedbekov <mamedbekovruslan@yandex.ru>
2025-01-20 17:33:26 +0300

    Базовая верстка главной страницы

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,14 +5,14 @@ export const Header = () => {
   return (
     <Flex w="100%" justifyContent="center" bg="#2E5077" padding={5}>
       <Box w="30%" display="flex" justifyContent="space-between">
-        <Link to="/cart" style={{ color: "white" }}>
-          Корзина
-        </Link>
         <Link to="/" style={{ color: "white" }}>
           Главная
         </Link>
         <Link to="/manage-product" style={{ color: "white" }}>
           Управление
+        </Link>
+        <Link to="/cart" style={{ color: "white" }}>
+          Корзина
         </Link>
       </Box>
       <Box position="absolute" right="5">
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,3 +1,23 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
+
 export const Home = () => {
-  return <div>Домашняя страница</div>;
+  return (
+    <Flex>
+      <Box>
+        <Text>Смартфоны</Text>
+        <Text>Телевизоры</Text>
+        <Text>Ноутбуки</Text>
+        <Text>Игровые консоли</Text>
+      </Box>
+      <Box>
+        <Text>Сортировки</Text>
+        <Flex>
+          <Box>Товар 1</Box>
+          <Box>Товар 2</Box>
+          <Box>Товар 3</Box>
+          <Box>Товар 4</Box>
+        </Flex>
+      </Box>
+    </Flex>
+  );
 };
